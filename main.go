32e9b71216fda19b1011da204dce8e1020376625
k5924 <kamranbhatti03072001@gmail.com>
2022-07-10 17:57:35 +0100

    Subject: Initial Commit Problem: No code in repo Solution: Created simple quiz app using go Note: Next commit will involve making the application more complex

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main(){
+  csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+  flag.Parse()
+
+  file, err := os.Open(*csvFilename)
+  if err != nil {
+    exit(fmt.Sprintf("Failed to open the CSV file: %s", *csvFilename))
+  }
+
+  r := csv.NewReader(file)
+  lines, err := r.ReadAll()
+  if err != nil {
+    exit("Failed to parse the provided CSV file.")
+  }
+
+  problems := parseLines(lines)
+
+  correct := 0
+
+  for i, problem := range problems {
+    fmt.Printf("Problem #%d: %s = \n", i+1, problem.question)
+    var answer string
+    fmt.Scanf("%s\n", &answer)
+    if answer == problem.answer {
+      correct++
+    }
+  }
+
+  fmt.Printf("You scored %d out of %d", correct, len(problems))
+
+
+}
+
+func parseLines(lines [][]string) []problem{
+  ret := make([]problem, len(lines))
+  for i, line := range lines {
+    ret[i] = problem{
+      question: line[0],
+      answer: strings.TrimSpace(line[1]),
+    }
+  }
+  return ret
+}
+
+func exit(msg string){
+  fmt.Println(msg)
+  os.Exit(1)
+}
+
+type problem struct {
+  question string
+  answer string
+}
